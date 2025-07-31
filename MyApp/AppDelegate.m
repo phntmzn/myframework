178b2a523793d4c428b83499902e29de8affc0df
zoned out <126989297+phntmzn@users.noreply.github.com>
2025-07-30 20:02:28 -0500

    Add files via upload

---
+++ MyApp/AppDelegate.m
@@ -0,0 +1,18 @@
+
+
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+    NSLog(@"Application did finish launching.");
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+    NSLog(@"Application will terminate.");
+}
+
+@end
